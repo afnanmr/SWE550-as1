7bede4b74c1aa1f8a9cd4f726afcab55b5fd7a9d
Afnan <a.alrowaily@futurework.com.sa>
2021-11-06 16:39:22 +0300

    springBootProject

---
+++ src/main/java/com/example/angular/springbootangulardemo/SpringbootAngularDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.angular.springbootangulardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularDemoApplication.class, args);
+	}
+
+}
